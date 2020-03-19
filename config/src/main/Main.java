34343e4597e420650a38afd130b8a68a817c830c
Daniel Martin Ochoa <56157528+dmartinochoa@users.noreply.github.com>
2020-03-19 15:54:59 +0100

    cleaner variable names

---
+++ config/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import controlador.*;
+import vista.*;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException, IOException {
+		Vista vista = new Vista();
+		Control control = new Control();
+		vista.setC(control);
+		control.setV(vista);
+		vista.setVisible(true);
+	}
+}
